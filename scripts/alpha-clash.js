1580d5c6b127d7224978af12fed92c7be6d29055
tasib94 <rahi84974@gmail.com>
2024-02-17 10:30:24 +0600

    updated the score section

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -14,11 +14,31 @@ function handleKeyboradKeyUpEvent(event){
     const expectedAlphabet = currentAlphabet.toLowerCase();
     // console.log('you pressed:' ,playerPressed, 'expected:',expectedAlphabet);
     if(playerPressed === expectedAlphabet){
+
+        //step-1:  update the score
+        const currentScoreElement = document.getElementById('current-score');
+        const currentScoreText = currentScoreElement.innerText;
+        const currentScore = parseInt(currentScoreText);
+
+        // step-2: increase the score by 1
+        const newScore = currentScore + 1;
+        // step-3: show the new updated score
+        currentScoreElement.innerText = newScore;
+        // play again
         removeBackgroundColorById(expectedAlphabet);
         continueGame();
     }
     else{
-        console.log('you lost a life!!!')
+        // get the current life number
+        const currentLifeElement = document.getElementById('life');
+        const currentLifeText = currentLifeElement.innerText;
+        const currentLife = parseInt(currentLifeText);
+
+        // reduce the life count
+        const newLife = currentLife - 1;
+
+        // display the updated life count
+        currentLifeElement.innerText = newLife;
     }
 }
 // captured keyboard keypress
